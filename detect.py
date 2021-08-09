aff7a717cd3e90e3c4fe142af4b434248b370a39
Adam Howard <adamhoward@chromium.org>
2021-08-09 20:36:39 +0100

    print detections

--- detect.py
+++ detect.py
@@ -141,7 +141,8 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
                 
                 for box in det:
-                    print(box[1])
+                    tense = torch.tensor(box)
+                    print(tense.numpy()[1])
 
                 
 
